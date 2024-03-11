74b4eefb7a2fa7873a381bbf5af0e837700280ef
Jakub Neukirch <jakub.neukirch@toptal.com>
2024-03-11 15:08:03 +0100

    Setup navigation

--- app/src/main/java/tech/stonks/cryptoapp/MainActivity.kt
+++ app/src/main/java/tech/stonks/cryptoapp/MainActivity.kt
@@ -10,34 +10,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import tech.stonks.cryptoapp.ui.page.home.HomePage
 import tech.stonks.cryptoapp.ui.theme.CryptoWalletTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             CryptoWalletTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    NavHost(startDestination = "home", navController = navController) {
+                        composable("home") {
+                            HomePage()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoWalletTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/tech/stonks/cryptoapp/ui/page/home/HomePage.kt
@@ -0,0 +1,9 @@
+package tech.stonks.cryptoapp.ui.page.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomePage() {
+    Text("Home Page")
+}
